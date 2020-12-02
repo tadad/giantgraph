44b09ca32b392ddf06fb775c8d2f0b668c6dd517
Drew Tada <drew.tada@gmail.com>
2020-12-01 20:05:19 -0500

    WIP styling home

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,21 @@ import { NavBar } from './NavBar';
 export default function Home() {
   return (
     <>
-      <Link to="/about"><h1>wikigraph</h1></Link>
-      <Route component={NavBar} />
+      <div className="row h-100">
+        <div className="col-lg-6 offset-md-3 my-auto">
+          <h1 className="text-center mb-3">Wikigraph</h1>
+          <Route component={NavBar} />
+          <p className="text-muted"><Link to="/about">about</Link></p>
+        </div>
+      </div>
+      <div className="row fixed-bottom">
+        <div className="col-lg-6 offset-md-3 text-center">
+          <p>
+            a drop from the
+            <a href="https://www.koodos.com"> koodos collective</a>
+          </p>
+        </div>
+      </div>
     </>
   );
 }
